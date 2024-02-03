138bf634566bdddb7d292cfa974858bd96aa1bd2
psyGamer <psygamer21@gmail.com>
2024-02-03 16:31:06 +0100

    Add a simple 3D version of the Batcher

---
+++ Source/TAS/Render/Batcher3D.cs
@@ -0,0 +1,245 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Celeste64.TAS.Render;
+
+/// <summary>
+/// A version of the <see cref="Batcher"/> in 3D instead of 2D.
+/// </summary>
+public class Batcher3D
+{
+    /// <summary>
+    /// Vertex Format of Batcher.Vertex
+    /// </summary>
+    private static readonly VertexFormat VertexFormat = VertexFormat.Create<Vertex>(
+        new VertexFormat.Element(0, VertexType.Float3, false),
+        new VertexFormat.Element(1, VertexType.Float2, false),
+        new VertexFormat.Element(2, VertexType.UByte4, true)
+    );
+
+    /// <summary>
+    /// The Vertex Layout used for Sprite Batching
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Vertex(Vec3 position, Vec2 texcoord, Color color) : IVertex
+    {
+        public Vec3 Pos = position;
+        public Vec2 Tex = texcoord;
+        public Color Col = color;
+
+        public readonly VertexFormat Format => VertexFormat;
+    }
+
+    private IntPtr vertexPtr = IntPtr.Zero;
+    private int vertexCount = 0;
+    private int vertexCapacity = 0;
+
+    private IntPtr indexPtr = IntPtr.Zero;
+    private int indexCount = 0;
+    private int indexCapacity = 0;
+
+    private readonly Mesh mesh = new();
+    private readonly Material material = new(Assets.Shaders["Sprite"]);
+    private bool dirty = false;
+
+    public void Line(Vec3 from, Vec3 to, Color color, float thickness = 1.0f) => Line(from, to, color, Matrix4x4.Identity, thickness);
+    public void Line(Vec3 from, Vec3 to, Color color, Matrix transform, float thickness = 0.1f)
+    {
+        var normal = (to - from).Normalized();
+        // var tangent = Vec3.Max(Vec3.Cross(normal, Vec3.UnitX), Vec3.Cross(normal, Vec3.UnitY)) * (thickness / 2.0f);
+        var tangent = normal.Z < normal.X ? new Vec3(-normal.Y, normal.X, 0.0f) : new Vec3(0.0f, -normal.Z, normal.Y);
+        var bitangent = Vec3.Cross(normal, tangent);
+
+        tangent *= thickness / 2.0f;
+        bitangent *= thickness / 2.0f;
+
+        Box(from - tangent - bitangent, from - tangent + bitangent, from + tangent - bitangent, from + tangent + bitangent,
+            to - tangent - bitangent, to - tangent + bitangent, to + tangent - bitangent, to + tangent + bitangent,
+            color, transform);
+    }
+
+    /// <summary>
+    /// Renders a box of a solid color.
+    /// </summary>
+    /// <param name="v0">Front Top Left</param>
+    /// <param name="v1">Front Top Right</param>
+    /// <param name="v2">Front Bottom Left</param>
+    /// <param name="v3">Front Bottom Right</param>
+    /// <param name="v4">Back Top Left</param>
+    /// <param name="v5">Back Top Right</param>
+    /// <param name="v6">Back Bottom Left</param>
+    /// <param name="v7">Back Bottom Right</param>
+    /// <param name="color">Box color</param>
+    public void Box(Vec3 v0, Vec3 v1, Vec3 v2, Vec3 v3,
+                    Vec3 v4, Vec3 v5, Vec3 v6, Vec3 v7,
+                    Color color, Matrix transform)
+    {
+        EnsureVertexCapacity(vertexCount + 8); // 8 corners
+        EnsureIndexCapacity(indexCount + 36); // 6 sides * 2 triangles * 3 vertices
+
+        unsafe
+        {
+            Span<Vertex> vertices = new((Vertex*)vertexPtr + vertexCount, 8);
+            Span<int> indices = new((int*)indexPtr + indexCount, 36);
+
+            vertices[0].Pos = Vec3.Transform(v0, transform);
+            vertices[1].Pos = Vec3.Transform(v1, transform);
+            vertices[2].Pos = Vec3.Transform(v2, transform);
+            vertices[3].Pos = Vec3.Transform(v3, transform);
+            vertices[4].Pos = Vec3.Transform(v4, transform);
+            vertices[5].Pos = Vec3.Transform(v5, transform);
+            vertices[6].Pos = Vec3.Transform(v6, transform);
+            vertices[7].Pos = Vec3.Transform(v7, transform);
+            vertices[0].Col = color;
+            vertices[1].Col = color;
+            vertices[2].Col = color;
+            vertices[3].Col = color;
+            vertices[4].Col = color;
+            vertices[5].Col = color;
+            vertices[6].Col = color;
+            vertices[7].Col = color;
+
+            // Front
+            indices[0] = vertexCount + 0;
+            indices[1] = vertexCount + 2;
+            indices[2] = vertexCount + 1;
+            indices[3] = vertexCount + 2;
+            indices[4] = vertexCount + 3;
+            indices[5] = vertexCount + 1;
+            // Back
+            indices[6] = vertexCount + 4;
+            indices[7] = vertexCount + 5;
+            indices[8] = vertexCount + 6;
+            indices[9] = vertexCount + 5;
+            indices[10] = vertexCount + 7;
+            indices[11] = vertexCount + 6;
+            // Left
+            indices[12] = vertexCount + 0;
+            indices[13] = vertexCount + 6;
+            indices[14] = vertexCount + 2;
+            indices[15] = vertexCount + 0;
+            indices[16] = vertexCount + 4;
+            indices[17] = vertexCount + 6;
+            // Right
+            indices[18] = vertexCount + 1;
+            indices[19] = vertexCount + 3;
+            indices[20] = vertexCount + 7;
+            indices[21] = vertexCount + 1;
+            indices[22] = vertexCount + 7;
+            indices[23] = vertexCount + 5;
+            // Top
+            indices[24] = vertexCount + 0;
+            indices[25] = vertexCount + 1;
+            indices[26] = vertexCount + 5;
+            indices[27] = vertexCount + 0;
+            indices[28] = vertexCount + 5;
+            indices[29] = vertexCount + 4;
+            // Bottom
+            indices[30] = vertexCount + 2;
+            indices[31] = vertexCount + 7;
+            indices[32] = vertexCount + 6;
+            indices[33] = vertexCount + 2;
+            indices[34] = vertexCount + 3;
+            indices[35] = vertexCount + 7;
+        }
+
+        vertexCount += 8;
+        indexCount += 36;
+        dirty = true;
+    }
+
+    /// <summary>
+    /// Draws the Batcher3D to the given Target with the given RenderState
+    /// </summary>
+    public void Render(ref RenderState state)
+    {
+        if (indexPtr == IntPtr.Zero || vertexPtr == IntPtr.Zero)
+            return;
+
+        // Upload our data if we've been modified since the last time we rendered
+        if (dirty)
+        {
+            mesh.SetIndices(indexPtr, indexCount, IndexFormat.ThirtyTwo);
+            mesh.SetVertices(vertexPtr, vertexCount, VertexFormat);
+            dirty = false;
+        }
+
+        if (material.Shader?.Has("u_matrix") ?? false)
+            material.Set("u_matrix", state.Camera.ViewProjection);
+        if (material.Shader?.Has("u_far") ?? false)
+            material.Set("u_far", state.Camera.FarPlane);
+        if (material.Shader?.Has("u_near") ?? false)
+            material.Set("u_near", state.Camera.NearPlane);
+        if (material.Shader?.Has("u_texture") ?? false)
+            material.Set("u_texture", Assets.Textures["white"]);
+
+        var call = new DrawCommand(state.Camera.Target, mesh, material)
+        {
+            // BlendMode = BlendMode.Screen,
+            DepthCompare = state.DepthCompare,
+            DepthMask = state.DepthMask,
+            // DepthMask = false,
+            // DepthCompare = DepthCompare.Less,
+            CullMode = CullMode.None,
+            MeshIndexStart = 0,
+            MeshIndexCount = indexCount,
+        };
+        Log.Info($"Rendered {indexCount}");
+        call.Submit();
+        state.Calls++;
+        state.Triangles += indexCount / 3;
+    }
+
+    /// <summary>
+    /// Clears the Batcher3D.
+    /// </summary>
+    public void Clear()
+    {
+        vertexCount = 0;
+        indexCount = 0;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private unsafe void EnsureVertexCapacity(int capacity)
+    {
+        if (capacity < vertexCapacity) return;
+
+        if (vertexCapacity == 0)
+            vertexCapacity = 32;
+
+        while (capacity >= vertexCapacity)
+            vertexCapacity *= 2;
+
+        IntPtr newPtr = Marshal.AllocHGlobal(sizeof(Vertex) * vertexCapacity);
+
+        if (vertexCount > 0)
+            Buffer.MemoryCopy((void*)vertexPtr, (void*)newPtr, vertexCapacity * sizeof(Vertex), vertexCount * sizeof(Vertex));
+
+        if (vertexPtr != IntPtr.Zero)
+            Marshal.FreeHGlobal(vertexPtr);
+
+        vertexPtr = newPtr;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private unsafe void EnsureIndexCapacity(int capacity)
+    {
+        if (capacity < indexCapacity) return;
+
+        if (indexCapacity == 0)
+            indexCapacity = 32;
+
+        while (capacity >= indexCapacity)
+            indexCapacity *= 2;
+
+        IntPtr newPtr = Marshal.AllocHGlobal(sizeof(int) * indexCapacity);
+
+        if (indexCount > 0)
+            Buffer.MemoryCopy((void*)indexPtr, (void*)newPtr, indexCapacity * sizeof(int), indexCount * sizeof(int));
+
+        if (indexPtr != IntPtr.Zero)
+            Marshal.FreeHGlobal(indexPtr);
+
+        indexPtr = newPtr;
+    }
+}
